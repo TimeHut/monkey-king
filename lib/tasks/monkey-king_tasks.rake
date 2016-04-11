407fbee98464bb8955ad440ab1af97e88dc07f6e
Acen <acenqiu@gmail.com>
2016-04-11 16:36:41 +0800

    移除没用的测试

--- lib/tasks/monkey-king_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :monkey-king do
-#   # Task goes here
-# end
--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'ffa5b821182e01e70a7d978ce0265838b144dfcab4c94c2d68e555942f3e7210748e0c486be356b11fbb50792576622fbb5250f6d87f101fb65875de77818c2e'
