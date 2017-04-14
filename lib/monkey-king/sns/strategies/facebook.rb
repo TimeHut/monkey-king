700e61997d591ef13b0a6f72d6d8c73b236b211c
halfblood369 <halfblood369@gmail.com>
2017-04-14 12:16:39 +0800

    add friends api

--- lib/monkey-king/sns/strategies/facebook.rb
+++ lib/monkey-king/sns/strategies/facebook.rb
@@ -20,10 +20,14 @@ module MonkeyKing
 
         def self.friends uid, options={}
           url = "https://graph.facebook.com/v2.8/#{uid}/friends"
-          options[:access_token] = extend_token
+          options{:access_token} = extend_token
           query = (options.collect {|k, v| "#{k}=#{v}" }).join('&')
 
           query.present? ? "#{url}?#{query}" : url
+        end
+
+        def self.extend_token
+          new.extend_token
         end
 
         def permissions
